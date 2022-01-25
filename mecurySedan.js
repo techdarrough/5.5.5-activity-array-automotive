6bba57bd4a437ba851bb3d45143bbff7496fa87f
Preston Darrough <techdarrough@gmail.com>
2022-01-25 14:28:44 -0800

    trouble shoot activity bugs in solution code

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,13 +1,14 @@
-import Vehicle from "./vehicle.js";
+import Vehicle from "./vehicleBaseClass.js";
 
 class Car extends Vehicle {
-    constructor(make, model, year, colo, mileage) {
+    constructor(make, model, year, color, mileage) {
         super(make, model, year, color, mileage);
         this.maxPassengers = 5;
         this.passenger = 0;
         this.numberOfWheels = 4;
+        this.maxSpeed = 160;
         this.fuel = 10;
-        this.scheduledService = false;
+        this.scheduleService = false;
     }
 
     start ()
--- vehicleBaseClass.js
+++ vehicleBaseClass.js
@@ -86,6 +86,10 @@ class Vehicle {
 }
 
 //This exports things you want to use from this "module", more info in readme
-module.exports = {
+export default {
     Vehicle
 }
+
+// module.exports = {
+//     Vehicle
+// }
